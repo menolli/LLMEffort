22c3994c1d627305559f6164fe4d291233ce4781
menolli <menolli@uenp.edu.br>
2026-05-29 09:33:16 -0300

    Delete ProjectGood directory

--- ProjectGood/CreditCardPayment.java
+++
@@ -1,6 +0,0 @@
-public class CreditCardPayment implements PaymentMethod {
-    @Override
-    public void pay(double amount) {
-        System.out.println("Pagando R$ " + amount + " com cartão de crédito.");
-    }
-}
